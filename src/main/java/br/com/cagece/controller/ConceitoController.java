ee979c62ee216a24e684502a978d5ecfaec40cf8
Elton Santos <eltonaxl@hotmail.com>
2017-08-31 13:58:31 -0300

    Commit all

--- src/main/java/br/com/cagece/controller/ConceitoController.java
+++ src/main/java/br/com/cagece/controller/ConceitoController.java
@@ -86,18 +86,6 @@ public class ConceitoController implements Serializable {
         
     }
     
-    /* DETALHAR CONCEITO - MODAL */
-    /*
-    public void detalharConceito(Conceito conceito){
-        System.out.println("----------------------------------- CARREGANDO DETALHES DO CONCEITO");
-        this.conceito = conceito;
-        EntityManager em = new JPAUtil().getEntityManager();
-        Conceito c = em.find(Conceito.class, conceito.getId());
-        em.close();
-        return c;
-    }*/
-    
-    
     public void carregarConceito(Conceito conceito){
         System.out.println("----------------------------------- CARREGANDO DADOS DO CONCEITO");
         this.conceito = conceito;
